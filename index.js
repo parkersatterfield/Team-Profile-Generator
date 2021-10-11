adde699d3bf6af19db9819818d5a71618b999c64
PSatt <parkers205@gmail.com>
2021-10-11 17:55:51 -0500

    file tree setup and class creation

---
+++ index.js
@@ -0,0 +1,17 @@
+// App imports
+const fs = require('fs');
+const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+
+// Prompt User for Team Inputs
+getInput = () => {
+    inquirer.prompt([
+        {
+            
+        }
+    ])
+}
+
---
+++ tests/Employee.test.js
@@ -0,0 +1,16 @@
+const Employee = require('../lib/Employee');
+
+describe("Employee", () => {
+    describe('Employee Parameters', () => {
+        it('should create a name field that is equal to the employee name input', () => {
+            const employeeName = 'John Rogers';
+            const employee = new Employee(name, id, email)
+            expect(employee.name).toEqual(employeeName);
+        })
+    })
+    describe('getName', () => {
+        it('should return the name passed in to the input field as the employee name', () => {
+            const 
+        })
+    })
+})
